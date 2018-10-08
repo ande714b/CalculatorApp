141ce3daa7c802056824ee2491f7090e5e971145
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2018-10-08 13:35:25 +0200

    Added Menus, Added add and sub to bindings, fixed netframework.

--- CalculatorApp/Bindings.cs
+++ CalculatorApp/Bindings.cs
@@ -12,7 +12,26 @@ namespace CalculatorApp
     {
         public void Call(string menuID)
         {
-            throw new NotImplementedException();
+            switch (menuID)
+            {
+                case "add":
+                    Console.WriteLine("Skriv de to tal som du ville lægge sammen: ");
+                    double add1 = double.Parse(Console.ReadLine());
+                    double add2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Calculator.Add(add1, add2));
+                    break;
+
+                case "subtract":
+                    Console.WriteLine("Skriv de to tal som du ville lægge sammen: ");
+                    double subtract1 = double.Parse(Console.ReadLine());
+                    double subtract2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(Calculator.Add(subtract1, subtract2));
+                    break;
+
+                default:
+                    Console.WriteLine("Menuen eksisterer ikke...");
+                    break;
+            }
         }
     }
 }
