2eef74f704383af8db028ba4436f4e0a77f28926
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2018-10-09 12:57:53 +0200

    Made some comments. Changed the result line for some cases.

--- CalculatorApp/Bindings.cs
+++ CalculatorApp/Bindings.cs
@@ -15,17 +15,35 @@ namespace CalculatorApp
             switch (menuID)
             {
                 case "add":
-                    Console.WriteLine("Skriv de to tal som du ville lægge sammen: ");
+                    Console.WriteLine("Skriv det første tal: ");
                     double add1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv det andet tal: ");
                     double add2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(Calculator.Add(add1, add2));
+                    Console.WriteLine(add1 + "+" + add2 + "=" + Calculator.Add(add1, add2)); //Prints the result in a pretty format
                     break;
 
                 case "subtract":
-                    Console.WriteLine("Skriv de to tal som du ville lægge sammen: ");
+                    Console.WriteLine("Skriv det første tal: ");
                     double subtract1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv det andet tal: ");
                     double subtract2 = double.Parse(Console.ReadLine());
-                    Console.WriteLine(Calculator.Add(subtract1, subtract2));
+                    Console.WriteLine(subtract1 + "-" + subtract2 + "=" + Calculator.Add(subtract1, subtract2));
+                    break;
+
+                case "multiply":
+                    Console.WriteLine("Skriv det første tal: ");
+                    double multiply1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv det andet tal: ");
+                    double multiply2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(multiply1 + "*" + multiply2 + "=" + Calculator.Multiply(multiply1, multiply2));
+                    break;
+
+                case "divide":
+                    Console.WriteLine("Skriv det tal du vil dividere: ");
+                    double divide1 = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Skriv det tal du vil dividere med: ");
+                    double divide2 = double.Parse(Console.ReadLine());
+                    Console.WriteLine(divide1 + "/" + divide2 + "=" + Calculator.Divide(divide1, divide2));
                     break;
 
                 case "sum":
